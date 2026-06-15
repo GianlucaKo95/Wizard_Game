b4f4233095333e53c3740881581cc05f05bcd0a7
GianlucaKo95 <gianluca.koehni@googlemail.com>
2026-06-15 23:50:59 +0200

    Ngix

--- wizard/run.sh
+++ wizard/run.sh
@@ -17,5 +17,8 @@ find /app/frontend/dist -name "*.js" \
     -exec sed -i "s|__SUPABASE_URL__|${SUPA_URL}|g" {} \; \
     -exec sed -i "s|__SUPABASE_ANON_KEY__|${SUPA_KEY}|g" {} \;
 
+# Test nginx config
+nginx -t 2>&1 && log "nginx config OK" || { log "nginx config FEHLER!"; nginx -t; exit 1; }
+
 log "Starte nginx auf Port 3043..."
 exec nginx -g "daemon off;"
